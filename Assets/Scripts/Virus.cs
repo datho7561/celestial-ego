96b5579d0a7e660da31cf5bfeb3069a4e70f743e
David Thompson <davidethompson@me.com>
2020-03-30 09:42:05 -0400

    Some more work on Virus and its controller

---
+++ Assets/Scripts/Virus.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Virus : MonoBehaviour
+{
+
+    public float DamageToInflict;
+    public float Speed;
+    public float Size;
+
+}
--- Assets/Scripts/VirusController.cs
+++ Assets/Scripts/VirusController.cs
@@ -11,6 +11,8 @@ public class VirusController : MonoBehaviour
     public float TimeToFindTarget;
     // This is how long the virus will move towards the player for when attacking
     public float TimeToAttackTarget;
+    // Instance of Virus that this is controlling
+    public Virus ControlledVirus;
 
     // Represents if the player is currently attacking a target
     private bool Attacking;
@@ -20,15 +22,31 @@ public class VirusController : MonoBehaviour
     void Start()
     {
         Attacking = false;       
+        TimeAttackingOrAcquiring = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // TODO:   
         // update the counter
-        // update if attacking or acquiring target
-        // if attacking, move in facing direction, regardless of the player location
-        // if acquiring, rotate to face the player
+        TimeAttackingOrAcquiring += Time.deltaTime;
+        if (Attacking) {
+            if (TimeAttackingOrAcquiring > TimeToAttackTarget) {
+                TimeAttackingOrAcquiring = 0;
+                Attacking = false;
+            }
+            // When attacking, move in the direction the virus is facing
+            // https://stackoverflow.com/questions/39809617/move-an-object-in-the-direction-it-is-facing-c-sharp#39809858
+            transform.position += transform.forward * Time.deltaTime * ControlledVirus.Speed;
+        } else {
+            if (TimeAttackingOrAcquiring > TimeToAttackTarget) {
+                TimeAttackingOrAcquiring = 0;
+                Attacking = false;
+            }
+            // If acquiring target, rotate to face the player
+            transform.forward = new Vector3(
+                transform.position.x - TargetCellularOrganism.transform.position.x,
+                transform.position.y - TargetCellularOrganism.transform.position.y, 0).normalized;
+        }
     }
 }
