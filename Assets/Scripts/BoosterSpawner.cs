750e1363de994e66b6bb327d0779b9ee0837dba3
Harsh5751 <42149411+Harsh5751@users.noreply.github.com>
2020-04-01 16:16:32 -0400

    Adding booster scripts

---
+++ Assets/Scripts/BoosterSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoosterSpawner : MonoBehaviour
+{
+    private float spawnSpeed = 10f;
+
+    void Start()
+    {
+        InvokeRepeating("Generate", 0, spawnSpeed);
+    }
+
+    void Generate()
+    {
+        int x = Random.Range(-50, 50);
+        int y = Random.Range(-50, 50);
+        Vector3 Position = new Vector3(x, y, 0);
+        Position.z = 0;
+        Instantiate(GetComponent<BoosterItem>().getSpeedBooster(), Position, Quaternion.identity);
+    }
+}
