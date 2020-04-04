769cc82b85d7e682c3118dd2fb1e8f13fa39dbf8
David Thompson <davidethompson@me.com>
2020-04-04 17:56:26 -0400

    Adding a grid and re-removing the origial Virus

--- Assets/Scripts/PlayerCellController.cs
+++ Assets/Scripts/PlayerCellController.cs
@@ -6,14 +6,24 @@ public class PlayerCellController : MonoBehaviour
 {
     private int proteinMoney;
 
+    private readonly float WorldSize = 100;
+
     void Update() {
         Vector3 Position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Position.z = transform.position.z;
-        transform.position = Vector3.MoveTowards(transform.position, Position, GetComponent<PlayerCellularOrganism>().getSpeed() * Time.deltaTime / transform.localScale.x);
+        transform.position = ClampWorld(Vector3.MoveTowards(transform.position, Position, GetComponent<PlayerCellularOrganism>().getSpeed() * Time.deltaTime / transform.localScale.x));
         proteinMoney = GetComponent<EntityController>().getScore();
     }
 
     public int getMoney() {
         return proteinMoney;
+    }
+    
+    private Vector3 ClampWorld(Vector3 original)
+    {
+        return new Vector3(
+            Mathf.Clamp(original.x, -WorldSize, WorldSize),
+            Mathf.Clamp(original.y, -WorldSize, WorldSize),
+            original.z);
     }
 }
